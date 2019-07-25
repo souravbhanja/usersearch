800d728d0c494716b40a5ae352e5840b991ca053
souravbhanja <sourav@DESKTOP-PQ6V086.localdomain>
2019-07-25 16:30:13 +0530

    adding project

---
+++ src/containers/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+import './App.css';
+
+
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            robots: [],
+            searchField: ''
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response=> response.json())
+            .then(users => this.setState({robots: users}));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchField: event.target.value})
+    }
+
+    render(){
+        const { searchField, robots } = this.state; //Destructuring
+        const filterRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
+        })
+        if(!robots.length){
+            return <h1>Loading...</h1>
+        }else{
+            return (
+                <div className = 'tc' >
+                <h1 className="f2">RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <ErrorBoundry>
+                        <CardList robots = {filterRobots} />
+                    </ErrorBoundry>
+                </Scroll>
+                </div>
+            );
+        }
+    }
+}
+
+export default App;
---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+{
+    id: 1,
+    name: 'Shikhar Dhawan',
+    username: 'Gabbar',
+    email: 'gabbar@gmail.com'
+},
+{
+    id: 2,
+    name: 'Rohit Sharma',
+    username: 'Hitman',
+    email: 'hitman@gmail.com'
+},
+{
+    id: 3,
+    name: 'Virat Kohli',
+    username: 'Chiku',
+    email: 'chiku@gmail.com'
+},
+{
+    id: 4,
+    name: 'KL Rahul',
+    username: 'Rahul',
+    email: 'rahul@gmail.com'
+},
+{
+    id: 5,
+    name: 'MS Dhoni',
+    username: 'Mahi',
+    email: 'mahi@gmail.com'
+},
+{
+    id: 6,
+    name: 'Dinesh Kartik',
+    username: 'DK',
+    email: 'dk@gmail.com'
+},
+{
+    id: 7,
+    name: 'Hardik Pandya',
+    username: 'Pandya',
+    email: 'pandya@gmail.com'
+},
+{
+    id: 8,
+    name: 'Yuzi Chahal',
+    username: 'Yuzi',
+    email: 'yuzi@gmail.com'
+},
+{
+    id: 9,
+    name: 'Kuldeep Yadav',
+    username: 'Kuldeep',
+    email: 'kuldeep@gmail.com'
+},
+{
+    id: 10,
+    name: 'Jasprit Bumrah',
+    username: 'Jassi',
+    email: 'jassi@gmail.com'
+},
+
+]
